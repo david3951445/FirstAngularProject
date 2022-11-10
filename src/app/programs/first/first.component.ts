50e3497ca8e964b39c43bd2ba986dbb5134e1d8a
lama <david3951445@gmail.com>
2022-11-10 16:59:32 +0800

    add componet by cli

--- src/app/programs/first/first.component.ts
+++ src/app/programs/first/first.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.css']
 })
-export class firstComponent implements OnInit {
-  public item: any; //宣告一個名為item的any變數
+export class FirstComponent implements OnInit {
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
+
 }
