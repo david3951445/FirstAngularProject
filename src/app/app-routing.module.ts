2016217e05ee2a12ac1d12b98c874442cb9b4969
lama <david3951445@gmail.com>
2022-11-10 23:01:33 +0800

    Change global css to scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './programs/first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'first', component: FirstComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './programs/first/first.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FirstComponent,
   ],
   imports: [
     BrowserModule,
